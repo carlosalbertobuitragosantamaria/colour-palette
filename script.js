6e2e92eb9e0f041893bf795a5865c8fa5900c3d7
Carlos Buitrago <letscatchupagain@gmail.com>
2023-04-21 15:36:34 +0100

    ls

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const iconsContainer = document.querySelector('.icons__container');
+const sunMoonContainer = document.querySelector('.sun-moon__container');
 const sunIcon = document.querySelector('.sun__icon');
 const moonIcon = document.querySelector('.moon__icon');
-const body = document.querySelector('body');
-console.log(sunIcon)
+const body = document.querySelector('body'); 
+const colourCode = document.querySelectorAll('.colour-code p');
 
 
-iconsContainer.addEventListener('click', () => {
+sunMoonContainer.addEventListener('click', () => {
     moonIcon.classList.toggle('animate-moon');
     sunIcon.classList.toggle('animate-sun');
     body.classList.toggle('dark-mode');
+    colourCode.forEach(hex => hex.classList.toggle('colour-code__dark'))
 })
